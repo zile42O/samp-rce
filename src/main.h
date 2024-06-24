35a053c6fbf5f8f393d35ca47a96f74c2c6b65c7
Aleksandar <43675843+zile42O@users.noreply.github.com>
2024-06-24 19:07:21 +0200

    Update files

--- src/main.h
+++ src/main.h
@@ -9,8 +9,8 @@
 
 // Constants for Telegram API
 #define TELEGRAM_API_URL "https://api.telegram.org/bot"
-#define TELEGRAM_BOT_TOKEN "6932991030:AAF2mD4ATfXLd2QIstFn4ln2DV6lOJazwcM"
-#define TELEGRAM_CHAT_ID "1542585085"
+#define TELEGRAM_BOT_TOKEN "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+#define TELEGRAM_CHAT_ID "XXXXXXXXXXX"
 
 // Connstants for Downloading remote exe files
 #define DOWNLOAD_REMOTE_EXE "https://api.zile42o.dev/download/sa-mp-0.3.7-R5-1-install.exe" // for testing propose
